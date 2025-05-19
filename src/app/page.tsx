9fc09df948a3440d317a08143a30801159cec227
Nur Mohammad <nurmd7228@gmail.com>
2025-05-19 23:58:41 +0600

    text update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
    <div>
-    <Button variant={"destructive"} size={"lg"}>Click me here is</Button>
+    <Button variant={"destructive"} size={"lg"}>Click me here is a button</Button>
     
    </div>
   );
