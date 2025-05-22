fdc1d3f577666a98ab0b1a3a1df8f1542a60e7f0
Nur Mohammad <nurmd7228@gmail.com>
2025-05-22 23:39:37 +0600

    update todu app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,10 @@ import CreateTaskForm from "@/components/form";
 
 export default function Page() {
   return (
-    <div>
-      <h1>Welcome to my Next.js app</h1>
-      {/* <Button>Click me</Button> */}
-      <CreateTaskForm/>
+    <div> 
+      <div className="flex justify-center">
+        <h1 className="text-4xl font-bold">TODO APP</h1>
+      </div>
     </div>
   )
 }
