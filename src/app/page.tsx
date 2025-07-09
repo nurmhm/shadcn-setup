5d77aa4d34a1b2b3f1a7e314ee05fcbbefb9aa61
Nur Mohammad <nurmd7228@gmail.com>
2025-07-09 11:27:49 +0600

    feat: Add test paragraph to TODO APP page
    Inserted a temporary paragraph with the text 'helllo commmit' below the TODO APP heading for testing or demonstration purposes.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import CreateTaskForm from "@/components/form";
 
-
 export default function Page() {
   return (
-    <div> 
+    <div>
       <div className="flex justify-center">
         <h1 className="text-4xl font-bold">TODO APP</h1>
+        <p>helllo commmit</p>
       </div>
     </div>
-  )
+  );
 }
